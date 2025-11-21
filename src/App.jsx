603f013a8ccc750fa45a4fa45ffdb2be844d7226
Martina <martinatarantino02@gmail.com>
2025-11-21 22:06:16 +0100

    creating player page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   );
